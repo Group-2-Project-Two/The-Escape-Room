dcf6f6a39afaa88214f73dedd5f1bf711a2b48b0
flyingtadpole <archer.curry@northwestern.edu>
2023-11-02 19:12:31 -0500

    began story front/backend

--- controllers/api/story.js
+++ controllers/api/story.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const Story = require('../../models/story')
 
-  router.get('/story', async (req, res) => {
-    res.render('story');
-  });
+router.get('/story', async (req, res) => {
+  res.render('story');
+});
+
 
 module.exports = router;
