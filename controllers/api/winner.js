f0c5c766d04c8a222d8547d98c94736498f25c21
Scotty Jirik <jirikscotty@gmail.com>
2023-11-04 14:03:18 -0500

    trying to work on JS for winner page

--- controllers/api/winner.js
+++ controllers/api/winner.js
@@ -1,12 +1,25 @@
 const router = require('express').Router();
-
-const test = [
+const Story = require('../../models/story')
+/*const test = [
     {
       name: 'this is a test',
     },
   ];
   router.get('/winner', async (req, res) => {
     res.render('winner');
-  });
+  });*/
+
+  router.get('/winner', async (req, res) => {
+    try {
+      const winnerData = await Story.findOne({ where: { keyword: "winner" }})
+      // change query to not hardcode primary key
+      // update winner in seeds from tower to winner
+      console.log(winnerData)
+      const winner = winnerData.get({ plain: true})
+      res.render('winner', winner)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  })
 
 module.exports = router;
