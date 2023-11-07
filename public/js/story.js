7a098d546a87b34bd238d5dcc6c188566e51ae52
justmann <mj.justmann@gmail.com>
2023-11-07 09:22:55 -0600

    css-and-possible-begginging2

--- public/js/story.js
+++ public/js/story.js
@@ -1,45 +1,48 @@
 const buttons = document.querySelectorAll("button");
 
 function storySwitch(selected) {
-  console.log("selected text: ", selected.text)
-  console.log("made it to story switch")
+  console.log("selected text: ", selected.text);
+  console.log("made it to story switch");
   // if (selected.text === "Play Again?") {
   //   console.log("made it to place again")
   //   document.location.replace("/story")
   //   // change this to STORY 2
-  // } else 
+  // } else
   if (selected.text === "Quit while you're ahead?") {
-    console.log("made it to win")
-    location.replace("/winner")
+    console.log("made it to win");
+    location.replace("/winner");
   } else if (selected.text === "Give up?") {
-    console.log("made it to death")
-    location.replace("/death")
+    console.log("made it to death");
+    location.replace("/death");
   } else {
     document.location.replace("/story/continue");
   }
-} 
+}
 
 const moveStory = async function (event) {
-  const selected = {id: event.target.id, text: event.target.innerHTML};
-  if (selected.text === "Give up?" || selected.text === "Quit while you're ahead?") {
-    storySwitch(selected)
+  const selected = { id: event.target.id, text: event.target.innerHTML };
+  if (
+    selected.text === "Give up?" ||
+    selected.text === "Quit while you're ahead?"
+  ) {
+    storySwitch(selected);
   } else {
-    console.log("selects: ", selected)
-  try {
-    const response = await fetch("/story/continue", {
-      method: "POST",
-      body: JSON.stringify({ data: selected }),
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response.ok) {
-      console.log("Response ok! Would move page along now")
-      const result = await response.json()
-      // const storyType = result.type 
-      storySwitch(selected)
+    console.log("selects: ", selected);
+    try {
+      const response = await fetch("/story/continue", {
+        method: "POST",
+        body: JSON.stringify({ data: selected }),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        console.log("Response ok! Would move page along now");
+        const result = await response.json();
+        // const storyType = result.type
+        storySwitch(selected);
+      }
+    } catch (error) {
+      console.error("Error:", error);
     }
-  } catch (error) {
-    console.error("Error:", error);
-  }
   }
 };
 
@@ -61,7 +64,7 @@ function textTypeEffect(element, text, i = 0) {
     return;
   }
 
-  setTimeout(() => textTypeEffect(element, text, i + 1), 50);
+  setTimeout(() => textTypeEffect(element, text, i + 1), 60);
 }
 
 textTypeEffect(div, text);
