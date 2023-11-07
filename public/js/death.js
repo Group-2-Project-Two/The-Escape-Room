e3c52aecbda14c82880bd073181b84cac2fea02c
flyingtadpole <archer.curry@northwestern.edu>
2023-11-07 10:51:56 -0600

    added functionality to buttons on death and win pages

--- public/js/death.js
+++ public/js/death.js
@@ -1,1 +1,13 @@
-// identify elements on the page using document.getelementbyId etc
+const againButton = document.getElementById("again")
+const leaveButton = document.getElementById("leave")
+
+function moveToAgain() {
+    location.replace("/story")
+}
+
+function leavePage() {
+    location.replace("/")
+}
+
+againButton.addEventListener("click", moveToAgain)
+leaveButton.addEventListener("click", leavePage)
