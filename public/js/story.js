cf1b984847f6df4bb673ec9475982f54408792ff
flyingtadpole <archer.curry@northwestern.edu>
2023-11-07 13:37:07 -0600

    fixed button bug

--- public/js/story.js
+++ public/js/story.js
@@ -1,18 +1,9 @@
 const buttons = document.querySelectorAll("button");
 
 function storySwitch(selected) {
-  console.log("selected text: ", selected.text);
-  console.log("made it to story switch");
-  // if (selected.text === "Play Again?") {
-  //   console.log("made it to place again")
-  //   document.location.replace("/story")
-  //   // change this to STORY 2
-  // } else
   if (selected.text === "Quit while you're ahead?") {
-    console.log("made it to win");
     location.replace("/winner");
   } else if (selected.text === "Give up?") {
-    console.log("made it to death");
     location.replace("/death");
   } else {
     document.location.replace("/story/continue");
@@ -27,7 +18,6 @@ const moveStory = async function (event) {
   ) {
     storySwitch(selected);
   } else {
-    console.log("selects: ", selected);
     try {
       const response = await fetch("/story/continue", {
         method: "POST",
@@ -35,9 +25,7 @@ const moveStory = async function (event) {
         headers: { "Content-Type": "application/json" },
       });
       if (response.ok) {
-        console.log("Response ok! Would move page along now");
         const result = await response.json();
-        // const storyType = result.type
         storySwitch(selected);
       }
     } catch (error) {
